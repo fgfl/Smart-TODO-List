f8d91acfe9762cbefc9001d3c7ccfe94797ad5da
Frederick <frederick.gfl@gmail.com>
2020-01-14 04:11:18 +0000

    Add clicker event hooks for edit and delete

--- public/scripts/task_element.js
+++ public/scripts/task_element.js
@@ -1,16 +1,16 @@
 const createTaskElement = (task) => {
   const taskElm = `
-    <article class="task container-fluid">
+    <article class="task container">
       <div class="row">
         <i class="fas fa-film col col-1"></i>
-        <h3 class="task-name col col-8">task name</h3>
+        <h3 class="task-name col col-8">${task.taskName || 'default todo'}</h3>
         <i class="far fa-flag col col-1"></i>
         <i class="fas fa-edit col col-1"></i>
         <i class="fas fa-trash col col-1"></i>
       </div>
       <div class="row">
         <div class="col col-1"></div>
-        <p class="created-at col col-8">time</p>
+        <p class="created-at col col-8">${task.scheduledDate || 'default time'}</p>
         <div class="col col-1"></div>
         <div class="col col-1"></div>
         <div class="col col-1"></div>
@@ -26,14 +26,27 @@ const renderTaskElm = (task) => {
 
 const renderTaskElms = (taskArray) => {
   for (const task of taskArray) {
-    console.log('in loop')
     renderTaskElm(task);
   }
 };
 
+// == Async functions ==
+const editTask = function() {
+  console.log('edit task')
+};
+
+const deleteTask = function() {
+  console.log('delete task')
+};
+
+// == Document Ready ==
 $(document).ready(function() {
   renderTaskElms([
     {},
     {},
+    {},
   ]);
+
+  $('.task .fa-edit').click(editTask);
+  $('.task .fa-trash').click(deleteTask);
 });
