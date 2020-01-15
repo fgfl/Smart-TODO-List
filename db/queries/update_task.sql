5cf0dc0db3dc08054ae256858f95db2e12099e93
Mitch Lum <mklum88@gmail.com>
2020-01-15 22:11:53 +0000

    query to update task

---
+++ db/queries/update_task.sql
@@ -0,0 +1,12 @@
+  UPDATE tasks
+        SET
+          category_id = 1,
+          user_id = 1,
+          task_name = 'Something',
+          schedule_date = null,
+          completed_date = null,
+          priority = null,
+          details_url = null
+        FROM categories
+        WHERE tasks.id = 1 AND category_id = categories.id
+        RETURNING *;
