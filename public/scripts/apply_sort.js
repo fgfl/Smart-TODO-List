a10c69a3c84c70fdf09faefe54fb0c8fbe69d4b2
Frederick <frederick.gfl@gmail.com>
2020-01-16 18:57:09 +0000

    Add animation for all filters

--- public/scripts/apply_sort.js
+++ public/scripts/apply_sort.js
@@ -1,21 +1,35 @@
 const SLIDEOUT_TIME = 600;
 const REENTER_DELAY = 300;
+const SHOW_HIDE_DELAY = 500;
 
 const getTaskList = () => {
   return [...$('.task-list').find('.task')];
 };
 
-const animateSort = (taskList, categoryName) => {
+const getCompletedList = () => {
+  return getTaskList().filter((task) => $(task).data('completed_date'));
+};
 
-  const listToHide = getTaskList().filter((task) => $(task).data('category_name') !== categoryName);
+const getNotCompletedList = () => {
+  return getTaskList().filter((task) => !$(task).data('completed_date'));
+};
 
+const getHideListByCategory = (categoryName) => {
+  return getTaskList().filter((task) => $(task).data('category_name') !== categoryName);
+};
+
+const getShowListByCategory = (categoryName) => {
+  return getTaskList().filter((task) => $(task).data('category_name') === categoryName);
+};
+
+const animateSort = (taskList, listToHide, listToShow) => {
   const slideOut = taskList
                     .animate({
                       'translate': '-100%',
                     }, SLIDEOUT_TIME)
                     .promise()
 
-  const hide = $(listToHide).hide(500).promise();
+  const hide = $(listToHide).hide(SHOW_HIDE_DELAY).promise();
 
   $.when(slideOut, hide).then(function() {
     setTimeout(() => {
@@ -24,55 +38,52 @@ const animateSort = (taskList, categoryName) => {
           'translate': '0%',
         }, SLIDEOUT_TIME)
         .promise();
+
+      $(listToShow).show(SHOW_HIDE_DELAY).promise();
     }, REENTER_DELAY);
   });
 };
 
 const applyAllSort = function() {
-  getTaskList().sort()
+  animateSort(
+    $('.task-list'),
+    [],
+    getTaskList());
 };
 
 const applyWatchSort = function() {
-  console.log('asdf')
-  animateSort($('.task-list'), 'read')
-  // const buyList = getTaskList().filter((task) => $(task).data('category_name') === 'watch');
-
-  // $('.task-list').animate({
-  //   'translate': '-100%'
-  // })
-  // .promise()
-  // .then(function() {
-  //   buyList.forEach(task => {
-  //     $(task).hide();
-  //   });
-
-  //   setTimeout(() => {
-  //     $('.task-list').animate({
-  //       'translate': '0%'
-  //     })},
-  //     300);
-  // });
-
+  animateSort(
+    $('.task-list'),
+    getHideListByCategory('watch'),
+    getShowListByCategory('watch'));
 };
 
 const applyReadSort = function() {
-  console.log('asdf')
-
+  animateSort(
+    $('.task-list'),
+    getHideListByCategory('read'),
+    getShowListByCategory('read'));
 };
 
 const applyEatSort = function() {
-  console.log('asdf')
-
+  animateSort(
+    $('.task-list'),
+    getHideListByCategory('eat'),
+    getShowListByCategory('eat'));
 };
 
 const applyBuySort = function() {
-  console.log('asdf')
-
+  animateSort(
+    $('.task-list'),
+    getHideListByCategory('buy'),
+    getShowListByCategory('buy'));
 };
 
 const applyCompletedSort = function() {
-  console.log('asdf')
-
+  animateSort(
+    $('.task-list'),
+    getNotCompletedList(),
+    getCompletedList());
 };
 
 // == DOCUMENT READY ==
